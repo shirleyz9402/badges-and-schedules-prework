6bcd30536ed02ae092ebb0839da0bd95be6d2447
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 19:28:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-# arr = []
-  speakers.each_with_index{|speakers,index| p "Hello #{speakers}! You'll be assigned to room #{index + 1}!"}
+ arr = []
+ arr.push(speakers.each_with_index{|speakers,index| p "Hello #{speakers}! You'll be assigned to room #{index + 1}!"})
+ return arr 
 end 
