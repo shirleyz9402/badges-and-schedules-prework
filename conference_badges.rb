71bc8ea3905bb4519d397add0066019ec6626813
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 19:26:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,5 @@ end
 
 def assign_rooms(speakers)
 # arr = []
- speakers.each_with_index{|speakers,index| p "Hello #{speakers}! You'll be assigned to room #{index + 1}!"}
- return arr 
+ return speakers.each_with_index{|speakers,index| p "Hello #{speakers}! You'll be assigned to room #{index + 1}!"}
 end 
