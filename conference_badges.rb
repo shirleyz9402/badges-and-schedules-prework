a5b4aee0d723c510728cd3fca4b93202739aa2eb
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 19:10:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+  badge = []
+  names.each do |name|
+    badge.push("Hello, my name is #{name}.")
+  end 
+  return badge
+end
+
+def assign_rooms(speakers)
